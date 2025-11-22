ec3d73f25165f560ecaa81a43c4cc6a401686145
Aryan Verma <arvee4796@gmail.com>
2025-11-21 20:46:37 -0600

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.py
@@ -0,0 +1,15 @@
+# Last updated: 11/21/2025, 8:46:37 PM
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        
+        return -1

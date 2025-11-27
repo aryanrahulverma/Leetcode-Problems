1af637806da10765148848a913cf1e390cc79ca4
Aryan Verma <arvee4796@gmail.com>
2025-11-27 12:31:36 -0600

    File updated at 11/27/2025, 12:31:36 PM

--- 226-InvertBinaryTree/226-InvertBinaryTree.py
+++ 226-InvertBinaryTree/226-InvertBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 11/27/2025, 12:31:31 PM
+# Last updated: 11/27/2025, 12:31:36 PM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, val=0, left=None, right=None):
@@ -10,13 +10,9 @@
 9        if not root:
 10            return None
 11
-12        # temp = root.left
-13        # root.left = root.right
-14        # root.right = temp
-15
-16        root.left, root.right = root.right, root.left
-17
-18        self.invertTree(root.left)
-19        self.invertTree(root.right)
-20
-21        return root
+12        root.left, root.right = root.right, root.left
+13
+14        self.invertTree(root.left)
+15        self.invertTree(root.right)
+16
+17        return root

a8e26402096e86b393fdd5d1df7eadebbb28011e
Aryan Verma <arvee4796@gmail.com>
2025-12-06 20:20:10 -0600

    File updated at 12/6/2025, 8:20:10 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,47 +1,29 @@
-# Last updated: 12/2/2025, 6:39:46 PM
-1# Definition for a binary tree node.
-2# class TreeNode:
-3#     def __init__(self, val=0, left=None, right=None):
-4#         self.val = val
-5#         self.left = left
-6#         self.right = right
-7class Solution:
-8    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-9        # res = []
-10
-11        # def dfs(root, d):
-12        #     if not root: 
-13        #         return None
-14            
-15        #     if len(res) == d:
-16        #         res.append([])
-17
-18        #     res[d].append(root.val)
-19        #     dfs(root.left, d + 1)
-20        #     dfs(root.right, d + 1)
-21        
-22        # dfs(root, 0)
-23
-24        # return res
-25
-26        q = []
-27        res = []
-28
-29        if not root:
-30            return []
-31
-32        q.append(root)
-33
-34        while q:
-35            temp = []
-36            for _ in range(len(q)):
-37                node = q.pop(0)
-38
-39                if node.left: q.append(node.left)
-40                if node.right: q.append(node.right)
-41
-42                temp.append(node.val)
-43            
-44            res.append(temp)
-45        
-46        return res
+# Last updated: 12/6/2025, 8:20:10 PM
+1class Solution:
+2    def lengthOfLongestSubstring(self, s: str) -> int:
+3        
+4        l, r = 0, 0
+5        length = 0
+6        seen = set()
+7
+8        if s == '':
+9            return 0
+10        
+11        while r < len(s):
+12            if s[r] in seen:
+13                while s[l] != s[r]:
+14                    seen.remove(s[l])
+15                    l += 1
+16                l += 1
+17                r += 1
+18            
+19            else:
+20                seen.add(s[r])
+21                length = max(length, r - l + 1)
+22                r += 1
+23    
+24        return length
+25            
+26
+27            
+28            

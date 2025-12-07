4a293a0c48bcde16d716367b5aa6363d7fb7f67e
Aryan Verma <arvee4796@gmail.com>
2025-12-06 22:45:58 -0600

    File updated at 12/6/2025, 10:45:58 PM

--- 1876-SubstringsofSizeThreewithDistinctCharacters/1876-SubstringsofSizeThreewithDistinctCharacters.py
+++ 1876-SubstringsofSizeThreewithDistinctCharacters/1876-SubstringsofSizeThreewithDistinctCharacters.py
@@ -1,37 +1,27 @@
-# Last updated: 12/6/2025, 9:57:18 PM
+# Last updated: 12/6/2025, 10:45:58 PM
 1# Definition for singly-linked list.
 2# class ListNode:
-3#     def __init__(self, x):
-4#         self.val = x
-5#         self.next = None
-6
-7class Solution:
-8    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-9        indexmp = {}
-10        ind = 0
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution:
+7    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+8        
+9        dummy = ListNode(0, head)
+10        p1, p2 = dummy, dummy
 11
-12        if not head:
+12        if p1.next == None:
 13            return None
-14
-15        slow = head
-16        indexmp[slow] = 0
-17
-18        while slow and slow.next:
-19            curr = slow
-20            slow = slow.next
-21            if slow not in indexmp:
-22                indexmp[slow] = ind
-23                ind += 1
-24                
-25            else:
-26                return slow
-27            
-28        return None
-29            
-30
-31            
-32
-33        
-34
-35
-36
+14        
+15        for _ in range(n):
+16            p2 = p2.next
+17        
+18        while p2 and p2.next:
+19            p1 = p1.next
+20            p2 = p2.next
+21        
+22        p1.next = p1.next.next
+23
+24        return dummy.next
+25
+26
